0015e801872a4fd831770c5c1c18b04014a337d6
Hydro <hydrozhao@gmail.com>
2020-04-13 11:31:44 +0800

    solving the fields type problem

--- filebeat/gem5-deploy.sh
+++ filebeat/gem5-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-touch $GEM5_PATH/m5out/tmp
-rm $GEM5_PATH/m5out/tmp
+curl -XDELETE 'http://localhost:9200/gem5-filebeat-*'
 export M5OUT_MODIFICATION_TIME=$(date +%Y.%m.%d'-'%H.%M.%S -r $GEM5_PATH/m5out)
 export M5OUT_MODIFICATION_TIME_FIELD=$(date -r $GEM5_PATH/m5out)
 rm -f data/registry/filebeat/data.json
